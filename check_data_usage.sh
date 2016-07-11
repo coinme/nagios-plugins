cd48045665059703e4e20e9511a7c8081743072a
Sebastian Imlay <sebastian.imlay@gmail.com>
2016-07-11 10:30:08 -0700

    Stupid permissions issue

--- check_data_usage.sh
+++ check_data_usage.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-NagiosPluginsDir=/usr/lib/nagios/plugins
-if [ ! -f $NagiosPluginsDir/LastDataOut.txt ]
+tmp=/tmp/
+if [ ! -f $tmp/LastDataOut.txt ]
 then
-	touch $NagiosPluginsDir/LastDataOut.txt
-	echo "0" > $NagiosPluginsDir/LastDataOut.txt
+	touch $tmp/LastDataOut.txt
+	echo "0" > $tmp/LastDataOut.txt
 fi
 
-CurrentDataOut=$(echo $(($(cat /proc/net/dev  | grep eth | awk '{print $2+$10}')/2**20)))
-LastDataOut=`cat $NagiosPluginsDir/LastDataOut.txt`
+CurrentDataOut=$(echo $(($(cat /proc/net/dev  | grep eth | awk '{print $2+$10}'))))
+LastDataOut=`cat $tmp/LastDataOut.txt`
 DataOut=$(($CurrentDataOut - $LastDataOut))
-echo $CurrentDataOut > $NagiosPluginsDir/LastDataOut.txt
+echo $CurrentDataOut > $tmp/LastDataOut.txt
 
 echo "Network dataout in last 5 minute is: $DataOut | DataOut=${DataOut}B;2048000;4096000;0;${DataOut}"
 exit 0
