da4d58b487abee6a8151c0321db3a29fff9851a1
Sebastian Imlay <sebastian.imlay@gmail.com>
2016-07-11 10:37:55 -0700

    Stupid permissions issue

--- check_data_usage.sh
+++ check_data_usage.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 tmp=/tmp/
-if [ ! -f $tmp/LastDataOut.txt ]
-then
-	touch $tmp/LastDataOut.txt
+if [ ! -f $tmp/LastDataOut.txt ]; then
 	echo "0" > $tmp/LastDataOut.txt
 fi
 
-CurrentDataOut=$(echo $(($(cat /proc/net/dev  | grep eth | awk '{print $2+$10}'))))
-LastDataOut=`cat $tmp/LastDataOut.txt`
+CurrentDataOut=$(($(cat /proc/net/dev  | grep eth | awk '{print $2+$10}')))
+
+LastDataOut="$(cat $tmp/LastDataOut.txt)"
 DataOut=$(($CurrentDataOut - $LastDataOut))
+
 echo $CurrentDataOut > $tmp/LastDataOut.txt
 
 echo "Network dataout in last 5 minute is: $DataOut | DataOut=${DataOut}B;2048000;4096000;0;${DataOut}"
